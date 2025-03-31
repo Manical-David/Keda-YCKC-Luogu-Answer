d1f140cab8a4b15bc5b414ab28a433e285a2e0b0
Manical-David <190215913@qq.com>
2025-03-31 15:03:57 +0000

    优化 P4665.cpp，使用数组代替向量存储正整数，并添加最小值查找逻辑

--- KedaOI/P4665.cpp
+++ KedaOI/P4665.cpp
@@ -1,17 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+constexpr int N = INT_MAX - 1;
+int tong[N];
 int main() {
     ios::sync_with_stdio(false), cin.tie(), cout.tie();
     int n;
     cin >> n;
-    vector <int> nums(n);
-    for (int i = 0; i < n; i++) {
-        int tong;
-        cin >> tong;
-        if(tong > 0) {
-            nums.push_back(tong);
+    for(int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        if(num > 0) {
+            tong[num] = 1;
         }
     }
-    sort(nums.begin(), nums.end());
+    int min = 0;
+    for(int i = 0; i < N && tong[i] == 0; i++) {
+        min = i;
+    }
+    min++;
+    for(int i = min; i < N; i++) {
+        if(tong)
+    }
     return 0;
 }

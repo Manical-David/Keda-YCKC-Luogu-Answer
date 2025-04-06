1c09c391c9cf4c2a90ea95111c4a94b15010e734
Manical-David <190215913@qq.com>
2025-04-06 02:14:03 +0000

    更改题目的代码，使其运行效率更高

--- KedaOI/P0907.cpp
+++ KedaOI/P0907.cpp
@@ -1,17 +1,16 @@
 #include  <bits/stdc++.h>
 using namespace std;
 int solve(vector<int>& b, vector<int>& add, int n, int m) {
-    sort(add.begin(), add.end()); // 对供暖器位置进行排序
-    int res = 0; // 最小加热半径初始化为0
+    sort(add.begin(), add.end());
+    int res = 0;
     for (int i = 0; i < n; i++) {
         int pos = b[i];
-        // 使用二分查找找到离当前房屋最近的供暖器
-        auto it = lower_bound(add.begin(), add.end(), pos);
-        int l1 = (it != add.end()) ? abs(*it - pos) : INT_MAX; // 距离右侧供暖器的距离
-        int r1 = (it != add.begin()) ? abs(*(it - 1) - pos) : INT_MAX; // 距离左侧供暖器的距离
-        res = max(res, min(l1, r1)); // 更新最小加热半径
+        auto it = lower_bound(add.begin(), add.end(), pos); // 2分
+        int l1 = (it != add.end()) ? abs(*it - pos) : INT_MAX;
+        int r1 = (it != add.begin()) ? abs(*(it - 1) - pos) : INT_MAX;
+        res = max(res, min(l1, r1));
     }
-    return res; // 返回最小加热半径
+    return res;
 }
 int main() {
     int n, m;

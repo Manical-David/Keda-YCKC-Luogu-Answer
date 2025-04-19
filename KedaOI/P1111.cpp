64f48fdeb90d568b08078ff6c1d4c677b9dc0070
Manical-David <204435097+Manical-David@users.noreply.github.com>
2025-04-19 13:32:33 +0000

    刷了两道💦题，我都快💤了

---
+++ KedaOI/P1111.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false), cin.tie(), cout.tie();
+    int n;
+    cin >> n;
+    string str;
+    for(int i = 0; i < n; i++) {
+        cin >> str;
+        cout << str << '\n';
+    }
+    return 0;
+}

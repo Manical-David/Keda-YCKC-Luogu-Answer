2bd60c38f518c2f9f66fc785099629ae4f3703cf
Manical-David <204435097+Manical-David@users.noreply.github.com>
2025-04-20 03:35:02 +0000

    Finished problem

--- KedaOI/pape.cpp
+++ KedaOI/pape.cpp
@@ -1,40 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-constexpr int N = 27;
-int n, v, weight[N], w[N];
+#define int long long
+constexpr int N = 107;
+int f[N];
+int dfs(int u) {
+    if(u == 1) return 1;
+    if(u == 2) return 2;
+    if(f[u] != 0) return f[u];
+    int res = 0;
+    res = dfs(u - 1) + dfs(u -2);
+    f[u] = res;
+}
 int main() {
     ios::sync_with_stdio(false), cin.tie(), cout.tie();
-    cin >> n >> v;
-    for(int i = 0; i < n; i++) {
-        cin >> weight[i] >> w[i];
-    }
-    int minn = 100;
-    for(int i = 0; i < (1 << n); i++) {
-        int t = 0, tmpv = 0;
-        for(int j = 0; j < n; j++) {
-            if((i >> j) & 1) {
-                t += w[j];
-                tmpv += weight[j];
-            }
-        }
-        if(tmpv <= v) {
-            minn = min(minn, abs(100 - t));
-        }
-    }
-    int res = 0;
-    for(int i = 0; i < (1 << n); i++) {
-        int t = 0, tmpv = 0, cnt = 0;
-        for (int j = 0; j < n; j++) {
-            if((i >> j) & 1) {
-                t += w[j];
-                tmpv += weight[j];
-                cnt++;
-            }
-        }
-        if(tmpv <= v && abs(100 - t) == minn) {
-            res = max(res, cnt);
-        }
-    }
-    cout << res;
+    cout << dfs(100);
     return 0;
 }

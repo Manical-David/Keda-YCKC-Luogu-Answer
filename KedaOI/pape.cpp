ac4c42d439fb8314a79c5284d73d2c21d8da5cd4
Manical David <204435097+Manical-David@users.noreply.github.com>
2025-05-11 04:13:21 +0000

    ✅Accepted

--- KedaOI/pape.cpp
+++ KedaOI/pape.cpp
@@ -1,33 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-constexpr int N = 17, dir[2][2] = {{1, 0}, {0, 1}};
-int n, m, g[N][N];
-set<int> st;
-int res = 0;
-void dfs(int x, int y) {
-    if(x == n && y == m) {
+constexpr int dir[8][2] = {
+    {1, 2}, 
+    {2, 1}, 
+    {2, -1},
+    {1, -2},
+    {-1, -2},
+    {-2, -1},
+    {-2, 1}, 
+    {-1, 2}
+};
+constexpr int N = 17;
+int n, m, res = 0;
+bool st[N][N];
+void dfs(int x, int y, int cnt) {
+    if(cnt == n * m) {
         res += 1;
         return ;
     }
-    for(int i = 0; i < 2; i++) {
+    st[x][y] = true;
+    for (int i = 0; i < 8; i++) {
         int xx = x + dir[i][0], yy = y + dir[i][1];
-        if(xx < 1 || x > n || yy < 1 || yy > m) continue;
-        if(st.count(g[xx][yy])) continue;
-        st.insert(g[xx][yy]);
-        dfs(xx, yy);
-        st.erase(g[xx][yy]);
+        if(xx < 0 || xx >= n || yy < 0 || yy >= m) continue;
+        if(st[xx][yy]) continue;
+        dfs(xx, yy, cnt + 1);
     }
+    st[x][y] = false;
+    
 }
-int main() {
-    ios::sync_with_stdio(false), cin.tie(), cout.tie();
-    cin >> n >> m;
-    for(int i = 1; i <= n; i++)  {
-        for(int j = 1; j <= m; j++) {
-            cin >> g[i][j];
-        }
-    }
-    st.insert(g[1][1]);
-    dfs(1, 1);
+void solve() {
+    memset(st, false, sizeof(st));
+    int x, y;
+    cin >> n >> m >> x >> y;
+    res = 0;
+    dfs(x, y, 1);
     cout << res << '\n';
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
     return 0;
 }

c72db487813998543a7276b39613715febdf19d8
Manical David <204435097+Manical-David@users.noreply.github.com>
2025-05-16 07:44:57 +0000

    ✅Accepted

--- KedaOI/P1569.cpp
+++ KedaOI/P1569.cpp
@@ -1,1 +1,35 @@
-#include 
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 101;
+int a[N][N], b[N][N], c[N][N], ans[N][N];
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            cin >> b[i][j];
+        }
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            cin >> c[i][j];
+        }
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            ans[i][j] = a[i][j] - (b[i][j] - c[i][j]);
+        }
+    }
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            cout << ans[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}

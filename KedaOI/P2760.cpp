40e7d49c6bed7a860c284a6fd20a5bb5059f10da
Manical David <204435097+Manical-David@users.noreply.github.com>
2025-05-16 08:07:55 +0000

    ✅Accepted

---
+++ KedaOI/P2760.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false), cin.tie(), cout.tie();
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (a + b) * (c + d);
+    return 0;
+}

788d23d552edf0635a381e2e2399b76992ff6e9f
Manical David <204435097+Manical-David@users.noreply.github.com>
2025-05-16 08:03:38 +0000

    ✅Accepted

--- KedaOI/P2727.cpp
+++ KedaOI/P2727.cpp
@@ -1,1 +1,13 @@
-mod1
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y, z, Q;
+    cin >> x >> y >> z >> Q;
+    int tong = 0;
+    tong = (x * 2) + (y * 5) + (z * 3);
+    if(tong > Q) cout << "No\n";
+    else cout << "Yes\n";
+    cout << abs(tong - Q);
+    return 0;
+}

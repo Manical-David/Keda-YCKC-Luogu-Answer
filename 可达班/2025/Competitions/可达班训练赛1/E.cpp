8b7a218018726b7de9d316164bd399426f07e786
Manical David <204435097+Manical-David@users.noreply.github.com>
2025-05-25 08:07:39 +0000

    ✅Accepted

--- 可达班/2025/Competitions/可达班训练赛1/E.cpp
+++ 可达班/2025/Competitions/可达班训练赛1/E.cpp
@@ -1,68 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-int n, k;
-ll S;
-vector<ll> a;
-vector<ll> fact;
-ll times_(ll x) { // Mune
-    if (x > 20) return -1; // 20!爆
-    ll res = 1;
-    for (ll i = 2; i <= x; i++) {
-        res *= i;
-        if (res > S) return -1;
-    }
-    return res;
-}
-// 枚举
-void dfs(const vector<ll>& vals, unordered_map<ll, unordered_map<int, ll>>& mp) {
-    int m = (int)vals.size();
-    function<void(int, ll, int)> go = [&](int idx, ll sum, int f) { // Taught by 陈丁's vector
-        if (idx == m) {
-            mp[sum][f]++;
-            return;
-        }
-        ll val = vals[idx];
-        ll val_fact = times_(val);
-        go(idx + 1, sum, f);
-        go(idx + 1, sum + val, f);
-        if (val_fact != -1 && f + 1 <= k) {
-            go(idx + 1, sum + val_fact, f + 1);
-        }
-    };
-    go(0, 0, 0);
-}
 int main() {
-    ios::sync_with_stdio(false);
-    cin >> n >> k >> S;
-    a.resize(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    int mid = n / 2;
-    vector<ll> l(a.begin(), a.begin() + mid);
-    vector<ll> r(a.begin() + mid, a.end());
-    unordered_map<ll, unordered_map<int, ll> > lm, rm;
-    dfs(l, lm);
-    dfs(r, rm);
-    ll ans = 0;
-    for (auto it1 = lm.begin(); it1 != lm.end(); ++it1) {
-        ll sum_l = it1 -> first;
-        auto& used_map_l = it1 -> second;
-        ll tong = S - sum_l;
-        if (rm.count(tong)) {
-            auto& used_map_r = rm[tong];
-            for (auto it2 = used_map_l.begin(); it2 != used_map_l.end(); ++it2) {
-                int used_l = it2 -> first;
-                ll cnt_l = it2 -> second;
-                for (auto it3 = used_map_r.begin(); it3 != used_map_r.end(); ++it3) {
-                    int used_r = it3 -> first;
-                    ll cnt_r = it3 -> second;
-                    if (used_l + used_r <= k) {
-                        ans += cnt_l * cnt_r;
-                    }
+    int n, k, m;
+    cin >> n >> k >> m;
+    // dp[i][j]：处理到前i位，余数为j的方案数
+    // cnt[i][j]：处理到前i位，余数为j，且至少有一个后缀模k为0的方案数
+    vector<vector<int>> dp(n + 1, vector<int>(k, 0));
+    vector<vector<int>> cnt(n + 1, vector<int>(k, 0));
+    dp[0][0] = 1; // 初始状态：空数字的余数为0
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < k; ++j) {
+            if (dp[i - 1][j] == 0) continue;
+            for (int d = (i == 1 ? 1 : 0); d <= 9; ++d) { // 第一位不能为0
+                int new_j = (j * 10 + d) % k;
+                dp[i][new_j] = (dp[i][new_j] + dp[i - 1][j]) % m;
+                if (new_j == 0) {
+                    cnt[i][new_j] = (cnt[i][new_j] + dp[i - 1][j]) % m;
+                } else {
+                    cnt[i][new_j] = (cnt[i][new_j] + cnt[i - 1][j]) % m;
                 }
             }
         }
     }
-    cout << ans << "\n";
+    int ans = 0;
+    for (int j = 0; j < k; ++j) {
+        ans = (ans + cnt[n][j]) % m;
+    }
+    cout << ans << endl;
     return 0;
 }

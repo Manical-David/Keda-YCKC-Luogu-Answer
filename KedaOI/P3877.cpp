ff252d47da5e4bf715ebae6059625bf607cd8743
Manical David <204435097+Manical-David@users.noreply.github.com>
2025-07-09 12:31:55 +0000

    ❌ 25 Wrong Answer

--- KedaOI/P3877.cpp
+++ KedaOI/P3877.cpp
@@ -1,42 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 int main() {
     int n;
     string s;
     cin >> n >> s;
-    int mid = n;
-    string left = s.substr(0, mid), right = s.substr(mid, mid);
-    unordered_map<string, int> cnt;
-
-    // 枚举左半部分所有染色方案
-    for (int mask = 0; mask < (1 << mid); ++mask) {
+    string l = s.substr(0, n), r = s.substr(n, n);
+    unordered_map<string, int> mp;
+    for (int mask = 0; mask < (1 << n); ++mask) {
         string black, white;
-        for (int i = 0; i < mid; ++i) {
-            if (mask & (1 << i))
-                black += left[i];   // 染黑
-            else
-                white += left[i];   // 染白
+        for (int i = 0; i < n; ++i) {
+            if (mask & (1 << i)) black += l[i];
+            else white += l[i];
         }
-        cnt[black + "#" + white]++; // 统计每种方案出现次数
+        mp[black + "|" + white]++;
     }
-
     long long ans = 0;
-    // 枚举右半部分所有染色方案
-    for (int mask = 0; mask < (1 << mid); ++mask) {
+    for (int mask = 0; mask < (1 << n); ++mask) {
         string black, white;
-        for (int i = 0; i < mid; ++i) {
-            if (mask & (1 << i))
-                black += right[i];  // 染黑
-            else
-                white += right[i];  // 染白
+        for (int i = 0; i < n; ++i) {
+            if (mask & (1 << i)) black += r[i];
+            else white += r[i];
         }
-        string S1 = black;
-        string S2 = white;
-        reverse(S2.begin(), S2.end()); // 白色部分要反转
-        string key = S2 + "#" + S1;    // 构造与左半部分匹配的key
-        if (cnt.count(key))
-            ans += cnt[key];
+        reverse(white.begin(), white.end());
+        string key = white + "|" + black;
+        if (mp.count(key)) ans += mp[key];
     }
     cout << ans << endl;
     return 0;

0add16c289cdcafaf86f1759bb1658449fe166f8
Manical David <204435097+Manical-David@users.noreply.github.com>
2025-07-30 16:27:09 +0800

    ❌ 70 Wrong Answer

---
+++ YCKC/YC66.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false), cin.tie(), cout.tie();
+    int a;
+    cin >> a;
+    if(a <= 60) cout << "mei chui niu";
+    else cout << "niu niu chui shang tian";
+    return 0;
+}

8a0cbff4a445b186a7e1e291a4584c2fbf92059c
Manical David <204435097+Manical-David@users.noreply.github.com>
2025-07-30 16:29:28 +0800

    ✅Accepted

---
+++ YCKC/YC78.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false), cin.tie(), cout.tie();
+    string n;
+    cin >> n;
+    cout << (bool)(n.size() == 2);
+    return 0;
+}

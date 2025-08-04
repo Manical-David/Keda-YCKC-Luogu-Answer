0a9f9597c978f7512c2c9bf54db6d7564b3ccc0c
Manical David <204435097+Manical-David@users.noreply.github.com>
2025-08-04 21:12:58 +0800

    ✅Accepted

--- CodeForces/2044D.cpp
+++ CodeForces/2044D.cpp
@@ -1,16 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
+constexpr int N = 2e5 + 5;
+long long t, vis[N], cnt, n, a[N];
+void solve() {
+    memset(vis, 0, sizeof(vis));
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    for(int i = 1; i <= n; i++) {
+        if(vis[a[i]] == 1) cnt++;
+        else {
+            vis[a[i]] = 1;
+            cout << a[i] << ' ';
+        }
+    }
+    for(int i = 1; i <= n; i++) {
+        if(vis[i] != 1) cout << i << ' ';
+    }
+}
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int t;
     cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<int> a(n), b(n);
-        for (int i = 0; i < n; ++i) cin >> a[i];
-        for (int i = 0; i < n; ++i) cout << a[i] << " ";
-        cout << "\n";
+    while(t--) {
+        solve();
     }
     return 0;
 }

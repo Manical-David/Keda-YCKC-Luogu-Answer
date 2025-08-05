2da317ca82dd10504a34653ef76906f18e8ea2a6
Manical David <204435097+Manical-David@users.noreply.github.com>
2025-08-05 17:33:17 +0800

    ✅Accepted

---
+++ YCKC/YC74.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int n;
+    cin >> n;
+    if(n == 7 || n == 5 || n == 3) cout << "YES";
+    else cout << "NO";
+    return 0;
+}
---
+++ YCKC/YC79.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int a, b;
+    cin >> a >> b;
+    if(a >= 10 || b >= 20) cout << true;
+    else cout << false;   
+    return 0;
+}

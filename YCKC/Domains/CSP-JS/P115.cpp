9b2e27f85d93cb9f1f43402300ad84bc62e5ce63
Manical-David <204435097+Manical-David@users.noreply.github.com>
2025-08-16 09:18:55 +0800

    ✅Accepted

---
+++ YCKC/Domains/CSP-JS/P115.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    // ios::sync_with_stdio(false), cin.tie(0);
+    int n, c;
+    // cin >> n >> c;
+    scanf("%d%d", &n, &c);
+    vector <int> a(n, 0);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", a[i]);
+    }
+    
+    return 0;
+}
